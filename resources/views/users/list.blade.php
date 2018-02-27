64c7a3c03d492d3404ad2f3323e7738fab9ac12a
Unknown <jerome-fabricante@isuzuphil.com>
2018-02-27 09:58:29 +0800

    List of users markup

---
+++ resources/views/users/list.blade.php
@@ -0,0 +1,174 @@
+@extends('template')
+
+@section('aside-left')
+	<li class="m-menu__item m-menu__item--active" aria-haspopup="true" >
+		<a href="{{ route('users.index') }}" class="m-menu__link ">
+			<i class="m-menu__link-icon flaticon-users"></i>
+			<span class="m-menu__link-title">
+				<span class="m-menu__link-wrap">
+					<span class="m-menu__link-text">
+						Users
+					</span>
+				</span>
+			</span>
+		</a>
+	</li>
+@endsection
+
+@section('content')
+	<div class="m-content">
+		<div class="row">
+			<div class="col-md-12">
+				<div class="m-portlet m-portlet--mobile">
+					<div class="m-portlet__head">
+						<div class="m-portlet__head-caption">
+							<div class="m-portlet__head-title">
+								<span class="m-portlet__head-icon">
+									<i class="flaticon-users"></i>
+								</span>
+
+								<h3 class="m-portlet__head-text">
+									List of Users
+								</h3>
+							</div>
+						</div>
+
+						<div class="m-portlet__head-tools">
+							<ul class="m-portlet__nav">
+								<li class="m-portlet__nav-item">
+									<div class="m-dropdown m-dropdown--inline m-dropdown--arrow m-dropdown--align-right m-dropdown--align-push" data-dropdown-toggle="hover" aria-expanded="true">
+										<a href="#" class="m-portlet__nav-link btn btn-lg btn-secondary  m-btn m-btn--icon m-btn--icon-only m-btn--pill  m-dropdown__toggle">
+											<i class="la la-ellipsis-h m--font-brand"></i>
+										</a>
+										<div class="m-dropdown__wrapper">
+											<span class="m-dropdown__arrow m-dropdown__arrow--right m-dropdown__arrow--adjust"></span>
+											<div class="m-dropdown__inner">
+												<div class="m-dropdown__body">
+													<div class="m-dropdown__content">
+														<ul class="m-nav">
+															<li class="m-nav__section m-nav__section--first">
+																<span class="m-nav__section-text">
+																	Quick Actions
+																</span>
+															</li>
+															<li class="m-nav__item">
+																<a href="{{ route('users.create') }}" class="m-nav__link">
+																	<i class="m-nav__link-icon flaticon-share"></i>
+																	<span class="m-nav__link-text">
+																		Create New User
+																	</span>
+																</a>
+															</li>
+															<li class="m-nav__item">
+																<a href="" class="m-nav__link">
+																	<i class="m-nav__link-icon flaticon-chat-1"></i>
+																	<span class="m-nav__link-text">
+																		Send Messages
+																	</span>
+																</a>
+															</li>
+															<li class="m-nav__item">
+																<a href="" class="m-nav__link">
+																	<i class="m-nav__link-icon flaticon-multimedia-2"></i>
+																	<span class="m-nav__link-text">
+																		Upload File
+																	</span>
+																</a>
+															</li>
+															<li class="m-nav__section">
+																<span class="m-nav__section-text">
+																	Useful Links
+																</span>
+															</li>
+															<li class="m-nav__item">
+																<a href="" class="m-nav__link">
+																	<i class="m-nav__link-icon flaticon-info"></i>
+																	<span class="m-nav__link-text">
+																		FAQ
+																	</span>
+																</a>
+															</li>
+															<li class="m-nav__item">
+																<a href="" class="m-nav__link">
+																	<i class="m-nav__link-icon flaticon-lifebuoy"></i>
+																	<span class="m-nav__link-text">
+																		Support
+																	</span>
+																</a>
+															</li>
+														</ul>
+													</div>
+												</div>
+											</div>
+										</div>
+									</div>
+								</li>
+							</ul>
+						</div>
+					</div>
+
+					<div class="m-portlet__body">
+						<!--begin: Search Form -->
+						<div class="m-form m-form--label-align-right m--margin-top-20 m--margin-bottom-30">
+							<div class="row align-items-center">
+								<div class="col-xl-8 order-2 order-xl-1">
+									<div class="form-group m-form__group row align-items-center">
+										<div class="col-md-4">
+											<div class="m-input-icon m-input-icon--left">
+												<input type="text" class="form-control m-input m-input--solid" placeholder="Search..." id="generalSearch">
+												<span class="m-input-icon__icon m-input-icon__icon--left">
+													<span>
+														<i class="la la-search"></i>
+													</span>
+												</span>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<!--end: Search Form -->
+
+						<!--begin: Datatable -->
+						<table class="m-datatable" id="html_table" width="100%">
+							<thead>
+								<tr>
+									<th title="Field #1">#</th>
+									<th title="Field #2">Username</th>
+									<th title="Field #3">Name</th>
+									<th title="Field #4">Dealer</th>
+									<th title="Field #5">Job Position</th>
+									<th title="Field #6">Status</th>
+									<th title="Field #7">Actions</th>
+								</tr>
+							</thead>
+							<tbody>
+								<tr>
+									<td>1</td>
+									<td>170707</td>
+									<td>Jerome Fabricante</td>
+									<td>Cavite</td>
+									<td>Technician</td>
+									<td>
+										<span class="m--font-bold m--font-success">Active</span>
+									</td>
+									<td>
+										<a href="#" class="m-portlet__nav-link btn m-btn m-btn--hover-accent m-btn--icon m-btn--icon-only m-btn--pill" title="View ">
+											<i class="la la-edit"></i>
+										</a>
+									</td>
+								</tr>
+							</tbody>
+						</table>
+						<!--end: Datatable -->
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+@endsection
+
+@section('scripts')
+	@parent
+	<script src="{{ asset('metronic_5.05/assets/demo/default/custom/components/datatables/base/html-table.js') }}" type="text/javascript"></script>
+@endsection
