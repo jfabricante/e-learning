8f8fc1b50587b35255922c81913e168172a4a1aa
Unknown <jerome-fabricante@isuzuphil.com>
2018-03-26 14:18:53 +0800

    Alias and providers

--- config/app.php
+++ config/app.php
@@ -145,6 +145,10 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /*
+         * Collective Services
+         */
+        Collective\Html\HtmlServiceProvider::class,
     ],
 
     /*
@@ -192,6 +196,8 @@ return [
         'URL'       => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View'      => Illuminate\Support\Facades\View::class,
+        'Form'      => Collective\Html\FormFacade::class,
+        'Html'      => Collective\Html\HtmlFacade::class,
 
     ],
 
