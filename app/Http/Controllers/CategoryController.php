f54b2b6068b23ec29f501282518ee353e6a60743
Unknown <jerome-fabricante@isuzuphil.com>
2018-04-13 11:50:20 +0800

    Category controller

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Auth;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+use App\Models\Category;
+
+class CategoryController extends Controller
+{
+	/**
+	 * Display a listing of the resource.
+	 *
+	 * @return \Illuminate\Http\Response
+	 */
+	public function index()
+	{
+		$data = array(
+			'categories' => Category::all()
+		);
+
+		return view('categories/list', $data);
+	}
+
+	/**
+	 * Show the form for creating a new resource.
+	 *
+	 * @return \Illuminate\Http\Response
+	 */
+	public function create()
+	{
+		return view('categories/form');
+	}
+
+	/**
+	 * Store a newly created resource in storage.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @return \Illuminate\Http\Response
+	 */
+	public function store(Request $request)
+	{
+		$category = new Category();
+		$category->name = $request->category;
+		$category->save();
+
+		Session::flash('msg', 'Category has been saved!');
+
+		return redirect()->route('categories.index');
+	}
+
+	/**
+	 * Display the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return \Illuminate\Http\Response
+	 */
+	public function show($id)
+	{
+		//
+	}
+
+	/**
+	 * Show the form for editing the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return \Illuminate\Http\Response
+	 */
+	public function edit($id)
+	{
+		//
+		$data = array(
+				'category' => Category::find($id)
+			);
+
+		return view('categories/form', $data);
+	}
+
+	/**
+	 * Update the specified resource in storage.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @param  int  $id
+	 * @return \Illuminate\Http\Response
+	 */
+	public function update(Request $request, $id)
+	{
+		$category       = Category::find($id);
+		$category->name = $request->category;
+		$category->save();
+
+		Session::flash('msg', 'Category has been updated!');
+
+		return redirect()->route('categories.index');
+	}
+
+	/**
+	 * Remove the specified resource from storage.
+	 *
+	 * @param  int  $id
+	 * @return \Illuminate\Http\Response
+	 */
+	public function destroy($id)
+	{
+		//
+	}
+}
