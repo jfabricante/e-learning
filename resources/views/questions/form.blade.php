c07d45ad374587714d49218023523592313493bb
Unknown <jerome-fabricante@isuzuphil.com>
2018-04-17 17:01:12 +0800

    Refactor

--- resources/views/questions/form.blade.php
+++ resources/views/questions/form.blade.php
@@ -47,48 +47,106 @@
 							</div>
 						</div>
 					</div>
-					{{-- @if(isset($category))
-						{!! Form::open(['route' => ['categories.update', 'id' => $category['id']], 'class' => 'm-form m-form--fit m-form--label-align-right', 'method' => 'patch']) !!}
-					@else --}}
+					@if(isset($question) && count($question))
+						{!! Form::open(['route' => ['questions.update', 'id' => $question->id], 'class' => 'm-form m-form--fit m-form--label-align-right', 'method' => 'patch']) !!}
+					@else
 						{!! Form::open(['route' => 'questions.store', 'class' => 'm-form m-form--fit m-form--label-align-right']) !!}
-					{{-- @endif --}}
+					@endif
 						<div class="m-portlet__body questions">
 							<div class="form-group m-form__group question__item">
 								<label for="question">
 									Question
 								</label>
 
-								<textarea class="form-control m-input m-input--square" id="question" name="question" rows="3" required></textarea>
+								<textarea class="form-control m-input m-input--square" id="question" name="question" rows="3" required>{{ (isset($question) && count($question)) ? $question->question : '' }}</textarea>
+
+								<input type="text" class="form-control m-input m-input--square m--hide sub_category_id" name="sub_category_id" value="{{ $id }}">
 
 								<label>Choices</label>
 								<ul type="a" class="choices" id="choices">
-									<li class="choice-item">
-										<div class="input-group m-form__group col-md-6">
-											<input type="text" class="form-control m-input m-input--square" name="choice[]">
-											<div class="input-group-append">
-												<span class="input-group-text">
-													<label class="m-radio m-radio--single m-radio--state m-radio--state-primary">
-														<input type="radio" name="answer" class="answer">
-														<span></span>
-													</label>
-												</span>
-											</div>
-										</div>
-									</li>
+									@if (isset($question) && count($question))
+										@foreach($question->choices as $row)
+											<li class="choice-item">
+												<div class="input-group m-form__group col-md-7">
+													<input type="text" class="form-control m-input m-input--square choice-input" required value="{{ $row->choice }}" name="choice[]">
+													<div class="input-group-append">
+														<span class="input-group-text">
+															<label class="m-radio m-radio--single m-radio--state m-radio--state-primary">
+																<input type="radio" name="answer" value="{{ $row->choice }}" {{ $question->answer == $row->choice ? 'checked' : '' }} class="answer" required>
+																<span></span>
+															</label>
+														</span>
+													</div>
 
-									{{-- <li class="choice-item">
-										<div class="input-group m-form__group col-md-6">
-											<input type="text" class="form-control m-input m-input--square" name="choice[]">
-											<div class="input-group-append">
-												<span class="input-group-text">
-													<label class="m-radio m-radio--single m-radio--state m-radio--state-primary">
-														<input type="radio" name="answer">
-														<span></span>
-													</label>
-												</span>
+													<i class="la la-remove"></i>
+												</div>
+											</li>
+										@endforeach
+									@else
+										<li class="choice-item">
+											<div class="input-group m-form__group col-md-7">
+												<input type="text" class="form-control m-input m-input--square choice-input" required name="choice[]">
+												<div class="input-group-append">
+													<span class="input-group-text">
+														<label class="m-radio m-radio--single m-radio--state m-radio--state-primary">
+															<input type="radio" name="answer" value="" class="answer" required>
+															<span></span>
+														</label>
+													</span>
+												</div>
+
+												<i class="la la-remove"></i>
 											</div>
-										</div>
-									</li> --}}
+										</li>
+
+										<li class="choice-item">
+											<div class="input-group m-form__group col-md-7">
+												<input type="text" class="form-control m-input m-input--square choice-input" required name="choice[]">
+												<div class="input-group-append">
+													<span class="input-group-text">
+														<label class="m-radio m-radio--single m-radio--state m-radio--state-primary">
+															<input type="radio" name="answer" class="answer" required>
+															<span></span>
+														</label>
+													</span>
+												</div>
+
+												<i class="la la-remove"></i>
+											</div>
+										</li>
+
+										<li class="choice-item">
+											<div class="input-group m-form__group col-md-7">
+												<input type="text" class="form-control m-input m-input--square choice-input" required name="choice[]">
+												<div class="input-group-append">
+													<span class="input-group-text">
+														<label class="m-radio m-radio--single m-radio--state m-radio--state-primary">
+															<input type="radio" name="answer" class="answer" required>
+															<span></span>
+														</label>
+													</span>
+												</div>
+
+												<i class="la la-remove"></i>
+											</div>
+										</li>
+
+										<li class="choice-item">
+											<div class="input-group m-form__group col-md-7">
+												<input type="text" class="form-control m-input m-input--square choice-input" required name="choice[]">
+												<div class="input-group-append">
+													<span class="input-group-text">
+														<label class="m-radio m-radio--single m-radio--state m-radio--state-primary">
+															<input type="radio" name="answer" class="answer" required>
+															<span></span>
+														</label>
+													</span>
+												</div>
+
+												<i class="la la-remove"></i>
+											</div>
+										</li>
+									@endif
 								</ul>
  
 							</div>
@@ -122,15 +180,36 @@
 			var $answer      = $('.answer');
 
 			$add_choices.on('click', function() {
-				$choice_item.clone().appendTo('#choices');
+
+				var	$markup = '<li class="choice-item">';
+						$markup += '<div class="input-group m-form__group col-md-7">';
+							$markup += '<input type="text" class="form-control m-input m-input--square choice-input" name="choice[]">';
+							$markup += '<div class="input-group-append">';
+								$markup += '<span class="input-group-text">';
+									$markup += '<label class="m-radio m-radio--single m-radio--state m-radio--state-primary">';
+										$markup += '<input type="radio" name="answer" class="answer">';
+										$markup +=	'<span></span>';
+									$markup += '</label>';
+								$markup += '</span>';
+							$markup += '</div>';
+							$markup += '<i class="la la-remove"></i>';
+							$markup += '</div>';
+					$markup += '</li>';
+
+				$('#choices').append($markup);
 			});
 
-			/*$answer.on('click', function() {
-				alert($(this).val());
-			});*/
+			$(document).on('click', 'input.answer', function() {
 
-			$(document).on('click', '.answer', function() {
-				alert($(this).val());
+				const $self   = $(this);
+				const $answer = $self.parent().parent().parent().siblings('input.choice-input').val();
+
+				$self.val($answer);
+				console.log($self.val());
+			});
+
+			$(document).on('click', '.choices .la-remove', function() {
+				$(this).closest('li').remove();
 			});
 
 		})();
