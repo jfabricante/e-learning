da0a47cf0b2c53fc0db44f3f8e594f3cb76d1a70
Unknown <jerome-fabricante@isuzuphil.com>
2018-04-19 14:15:08 +0800

    Change anchor value

--- resources/views/categories/list.blade.php
+++ resources/views/categories/list.blade.php
@@ -145,7 +145,7 @@
 													</a>
 													<div class="dropdown-menu dropdown-menu-right">
 														<a class="dropdown-item" href="{{ route('categories.edit', $category['id']) }}"><i class="la la-edit"></i> Edit Details</a>
-														<a class="dropdown-item" href="#"><i class="la la-leaf"></i> Update Status</a>
+														<a class="dropdown-item" href="{{ route('categories.show', $category['id']) }}"><i class="la la-gear"></i> Configure Exam</a>
 														<a class="dropdown-item" href="#"><i class="la la-print"></i> Generate Report</a>
 													</div>
 												</div>
