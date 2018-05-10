e42c2655fd8877827f5e4a613ada129c9b576e3d
Unknown <jerome-fabricante@isuzuphil.com>
2018-05-10 13:12:28 +0800

    Fix indention attach relationship

--- app/Models/SubCategoryItems.php
+++ app/Models/SubCategoryItems.php
@@ -6,18 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class SubCategoryItems extends Model
 {
-    // Connection
-    protected $connection = 'e_learning';
+	// Connection
+	protected $connection = 'e_learning';
 
-    // Table
-    protected $table = 'sub_category_items_tbl';
+	// Table
+	protected $table = 'sub_category_items_tbl';
 
-    // Fillable fields
-    protected $fillable = ['sub_category_id', 'items', 'exam_config_id'];
+	// Fillable fields
+	protected $fillable = ['sub_category_id', 'items', 'exam_config_id'];
 
-    // Exam config
-    public function examConfig()
-    {
-    	return $this->belongsTo('App\Models\ExamConfig');
-    }
+	public $timestamps = false;
+
+	// Exam config
+	public function examConfig()
+	{
+		return $this->belongsTo('App\Models\ExamConfig');
+	}
 }
