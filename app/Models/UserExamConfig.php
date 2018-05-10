a4c941c699bdff231e39449accf2de7553248ada
Unknown <jerome-fabricante@isuzuphil.com>
2018-05-10 13:14:08 +0800

    User exam configuration

---
+++ app/Models/UserExamConfig.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class UserExamConfig extends Model
+{
+	// Connection
+	protected $connection = 'e_learning';
+
+	// Table
+	protected $table = 'user_exam_config_tbl';
+
+	// Fillable fields
+	protected $fillable = ['user_id', 'config_id'];
+
+	// Exam questions
+	public function examQuestions()
+	{
+		return $this->hasMany('App\Models\UserExamQuestions', 'user_exam_config_id');
+	}
+
+	// Exam config
+	public function examConfig()
+	{
+		return $this->belongsTo('App\Models\ExamConfig', 'config_id');
+	}
+}
