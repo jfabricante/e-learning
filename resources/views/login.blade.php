206516109819e5b3dbd5bbc81ec88b5f32673973
Unknown <jerome-fabricante@isuzuphil.com>
2018-05-10 13:16:30 +0800

    Remove blocks of code

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -168,15 +168,6 @@ License: You must have a valid license purchased only from themeforest(the above
 								</div>
 							</form>
 						</div>
-						{{-- <div class="m-login__account">
-							<span class="m-login__account-msg">
-								Don't have an account yet ?
-							</span>
-							&nbsp;&nbsp;
-							<a href="javascript:;" id="m_login_signup" class="m-link m-link--light m-login__account-link">
-								Sign Up
-							</a>
-						</div> --}}
 					</div>
 				</div>
 			</div>
